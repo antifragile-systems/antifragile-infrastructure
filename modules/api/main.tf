dc58636ce95a4e14fc393cf0e11165a8d740b0ad
Hugo Freire <hfreire@exec.sh>
2018-05-19 16:55:09 +0200

    fix: minor tweaks

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -19,10 +19,32 @@ resource "aws_api_gateway_rest_api" "antifragile-infrastructure" {
   name = "${var.name}"
 }
 
+resource "aws_api_gateway_deployment" "antifragile-service" {
+  rest_api_id = "${aws_api_gateway_rest_api.antifragile-infrastructure.id}"
+  stage_name  = "production"
+}
+
+resource "aws_api_gateway_stage" "antifragile-infrastructure" {
+  stage_name    = "production"
+  rest_api_id   = "${aws_api_gateway_rest_api.antifragile-infrastructure.id}"
+  deployment_id = "${aws_api_gateway_deployment.antifragile-service.id}"
+}
+
 resource "aws_api_gateway_domain_name" "antifragile-infrastructure" {
   domain_name = "${local.hostname}"
 
   certificate_arn = "${module.certificate.aws_acm_certificate_arn}"
+}
+
+resource "aws_api_gateway_base_path_mapping" "antifragile-infrastructure" {
+  api_id      = "${aws_api_gateway_rest_api.antifragile-infrastructure.id}"
+  stage_name  = "production"
+  domain_name = "${local.hostname}"
+  base_path   = "/"
+
+  depends_on = [
+    "aws_api_gateway_stage.antifragile-infrastructure",
+  ]
 }
 
 resource "aws_route53_record" "antifragile-infrastructure" {
