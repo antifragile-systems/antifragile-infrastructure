39354f299d854be06d8c61ce0b23a1e7e810215b
Hugo Freire <hfreire@exec.sh>
2018-06-16 11:32:51 +0200

    refactor: change order of terraform stanza

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  region = "${var.aws_region}"
-}
-
 terraform {
   required_version = ">= 0.9.3, != 0.9.5"
 
   backend "s3" {}
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
 }
 
 module "network" {
