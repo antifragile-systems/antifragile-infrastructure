1dffa968b7c590cad7dcff86d3cf99412aa5083e
Hugo Freire <hfreire@exec.sh>
2018-07-20 11:28:38 +0200

    fix: remove https listener from loadbalancer

--- modules/cluster/modules/loadbalancer/main.tf
+++ modules/cluster/modules/loadbalancer/main.tf
@@ -18,17 +18,6 @@ resource "aws_security_group" "antifragile-infrastructure" {
     ]
   }
 
-  # https
-  ingress {
-    from_port = "443"
-    to_port   = "443"
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
   egress {
     from_port = 0
     to_port   = 0
@@ -65,37 +54,6 @@ resource "aws_alb_listener" "antifragile-infrastructure-0" {
   load_balancer_arn = "${aws_alb.antifragile-infrastructure.id}"
   port              = "80"
   protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_alb_target_group.antifragile-infrastructure.id}"
-  }
-}
-
-data "aws_route53_zone" "selected" {
-  name         = "${var.domain_name}."
-  private_zone = false
-}
-
-module "certificate" {
-  source = "../../../certificate"
-
-  name                = "${var.name}"
-  aws_route53_zone_id = "${data.aws_route53_zone.selected.id}"
-  domain_name         = "${var.domain_name}"
-
-  subject_alternative_names = [
-    "*.${var.domain_name}",
-  ]
-
-  aws_region = "${var.aws_region}"
-}
-
-resource "aws_alb_listener" "antifragile-infrastructure-1" {
-  load_balancer_arn = "${aws_alb.antifragile-infrastructure.id}"
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = "${module.certificate.aws_acm_certificate_arn}"
 
   default_action {
     type             = "forward"
