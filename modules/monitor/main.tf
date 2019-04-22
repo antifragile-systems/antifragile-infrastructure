56665cd80a15e46b2c0fb4d8dedc9ff98fa15f62
Hugo Freire <hfreire@exec.sh>
2019-04-22 10:55:37 +0200

    chore: add global cloudwatch group

--- modules/monitor/main.tf
+++ modules/monitor/main.tf
@@ -3,6 +3,13 @@ resource "aws_cloudwatch_log_group" "antifragile-infrastructure" {
   retention_in_days = 30
 }
 
+resource "aws_cloudwatch_log_group" "antifragile-infrastructure-1" {
+  provider = "aws.global"
+
+  name              = "${var.name}"
+  retention_in_days = 30
+}
+
 resource "aws_sns_topic" "antifragile-infrastructure" {
   provider = "aws.global"
 
