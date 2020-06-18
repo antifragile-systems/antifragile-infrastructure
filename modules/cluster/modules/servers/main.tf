233e88aa17ae549c053476e5fc6eeed3e3c61e0c
Hugo Freire <hfreire@exec.sh>
2020-06-18 09:15:58 +0200

    fix(servers): replace launch configuration with template

--- modules/cluster/modules/servers/main.tf
+++ modules/cluster/modules/servers/main.tf
@@ -30,19 +30,19 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "antifragile-infrastructure" {
-  role = aws_iam_role.antifragile-infrastructure.name
+  role       = aws_iam_role.antifragile-infrastructure.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 resource "aws_iam_instance_profile" "antifragile-infrastructure" {
   name_prefix = "${var.name}."
-  role = aws_iam_role.antifragile-infrastructure.name
+  role        = aws_iam_role.antifragile-infrastructure.name
 }
 
 resource "aws_security_group" "antifragile-infrastructure" {
   name_prefix = "${var.name}.servers."
   description = var.name
-  vpc_id = var.aws_vpc_id
+  vpc_id      = var.aws_vpc_id
 
   lifecycle {
     create_before_destroy = true
@@ -50,8 +50,8 @@ resource "aws_security_group" "antifragile-infrastructure" {
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
 
     cidr_blocks = [
       "0.0.0.0/0",
@@ -59,32 +59,52 @@ resource "aws_security_group" "antifragile-infrastructure" {
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     ipv6_cidr_blocks = [
       "::/0",
     ]
   }
 }
 
-resource "aws_launch_configuration" "antifragile-infrastructure" {
-  name_prefix = "${var.name}."
+resource "aws_launch_template" "servers" {
+  name_prefix = "${var.name}.servers."
 
-  security_groups = [
-    aws_security_group.antifragile-infrastructure.id,
-  ]
-
-  key_name = var.aws_ec2_public_key_name
-  image_id = var.aws_ec2_ami
+  image_id      = var.aws_ec2_ami
   instance_type = var.aws_ec2_instance_type
-  iam_instance_profile = aws_iam_instance_profile.antifragile-infrastructure.name
-  user_data = data.template_file.user_data.rendered
-  spot_price = "0.0102"
-  enable_monitoring = false
+  key_name      = var.aws_ec2_public_key_name
 
-  root_block_device {
-    volume_type = "standard"
+  user_data = base64encode(data.template_file.user_data.rendered)
+
+  vpc_security_group_ids = [
+    aws_security_group.antifragile-infrastructure.id ]
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.antifragile-infrastructure.name
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = 30
+      volume_type           = "standard"
+      delete_on_termination = true
+    }
+  }
+
+  instance_market_options {
+    market_type = "spot"
+
+    spot_options {
+      max_price          = "0.0102"
+      spot_instance_type = "one-time"
+    }
+  }
+
+  monitoring {
+    enabled = false
   }
 
   lifecycle {
@@ -93,14 +113,18 @@ resource "aws_launch_configuration" "antifragile-infrastructure" {
 }
 
 resource "aws_autoscaling_group" "antifragile-infrastructure" {
-  name = var.name
+  name_prefix = "${var.name}.servers."
+
+  min_size         = var.aws_autoscaling_group_min_size
+  max_size         = var.aws_autoscaling_group_max_size
+  desired_capacity = var.aws_autoscaling_group_desired_capacity
 
   vpc_zone_identifier = var.aws_vpc_private_subnet_ids
 
-  launch_configuration = aws_launch_configuration.antifragile-infrastructure.name
-  min_size = var.aws_autoscaling_group_min_size
-  max_size = var.aws_autoscaling_group_max_size
-  desired_capacity = var.aws_autoscaling_group_desired_capacity
+  launch_template {
+    id      = aws_launch_template.servers.id
+    version = "$Latest"
+  }
 
   lifecycle {
     create_before_destroy = true
